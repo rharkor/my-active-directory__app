aa05497fe502393b1a92c1c34bb2172a7546d01b
rharkor <louis@huort.com>
2023-07-08 22:55:41 +0200

    feat: add user context

---
+++ src/contexts/user.store.ts
@@ -0,0 +1,34 @@
+import { apiProfile } from '@/lib/auth-calls';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
+import { create } from 'zustand';
+import {
+  devtools,
+  // persist
+} from 'zustand/middleware';
+
+interface UserState {
+  profile:
+    | (typeof apiProfile extends (router: AppRouterInstance) => Promise<infer T>
+        ? T
+        : never)
+    | null;
+  loadProfile: (router: AppRouterInstance) => void;
+}
+
+export const useUserStore = create<UserState>()(
+  devtools(
+    // persist(
+    (set) => ({
+      profile: null,
+      loadProfile: async (router: AppRouterInstance) => {
+        //? load the profile
+        const profile = await apiProfile(router);
+        set({ profile });
+      },
+    }),
+    {
+      name: 'user-store',
+    },
+    // ),
+  ),
+);

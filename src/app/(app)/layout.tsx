78bd85c6b69c6b1d4618608b8062d52de6ae55fd
rharkor <louis@huort.com>
2023-07-08 22:55:57 +0200

    udpate: add init state for user context

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,10 @@
-import Navbar from '@/components/navbar';
+import Navbar from '@/app/(app)/components/navbar';
+import InitState from './components/init-state';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <InitState />
       <Navbar />
       {children}
     </>

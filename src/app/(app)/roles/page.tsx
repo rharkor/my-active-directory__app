38aac3c4c4dc76a5de379ceb47873d21d46d3d9c
rharkor <louis@huort.com>
2023-07-08 22:56:12 +0200

    fix: page height because of navbar

--- src/app/(app)/roles/page.tsx
+++ src/app/(app)/roles/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Roles() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex  flex-col items-center p-24">
       <h1 className="text-6xl font-bold">Roles</h1>
       <div className={cn('flex mt-12')}>
         <Link href="/">

d893b15520ec5f02cf808e7568c11e6be79c6b75
rharkor <louis@huort.com>
2023-07-08 19:00:13 +0200

    feat: add role page

---
+++ src/app/(app)/roles/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+export default function Roles() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-6xl font-bold">Roles</h1>
+      <div className={cn('flex mt-12')}>
+        <Link href="/">
+          <Button variant="default">Home</Button>
+        </Link>
+        <Link href="/service-accounts">
+          <Button variant="default" className="ml-6">
+            Service Accounts
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
